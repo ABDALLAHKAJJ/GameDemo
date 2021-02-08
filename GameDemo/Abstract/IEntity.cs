5deae057a33c566333e3d3ec40e9d3d98fd6d2eb
Abdallahkajj <busy.heart@live.com>
2021-02-08 17:27:33 +0300

    initialising

--- GameDemo/Abstract/IEntity.cs
+++ GameDemo/Abstract/IEntity.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace GameDemo.Entites
+namespace GameDemo.Abstract
 {
-    interface IEntity
+    internal interface IEntity
     {
     }
 }
--- GameDemo/Entites/Gamer.cs
+++ GameDemo/Entites/Gamer.cs
@@ -1,9 +1,9 @@
-﻿using GameDemo.Entites;
+﻿using GameDemo.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace GameDemo.Concrete
+namespace GameDemo.Entites
 {
     internal class Gamer : IEntity
     {
