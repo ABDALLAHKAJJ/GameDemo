6d8da75cfb60a8b5aa9f25e5031e21e8b3dd4294
Abdallahkajj <busy.heart@live.com>
2021-02-08 17:23:12 +0300

    Add project files.

---
+++ GameDemo/Concrete/Gamer.cs
@@ -0,0 +1,16 @@
+﻿using GameDemo.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameDemo.Concrete
+{
+    internal class Gamer : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public int Level { get; set; }
+    }
+}
